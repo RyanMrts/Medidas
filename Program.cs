4272b2c459e803f877e20e342e557b02e641931b
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-02 14:26:18 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double metros, centimetros, kilometros;
+
+Console.Write("Me diga a medida em metros desejada: ");
+metros = Convert.ToDouble(Console.ReadLine());
+
+centimetros = metros * 100;
+kilometros = metros / 1000;
+Console.WriteLine($"{centimetros} cm");
+Console.WriteLine($"{metros} m");
+Console.WriteLine($"{kilometros} Km");
+
